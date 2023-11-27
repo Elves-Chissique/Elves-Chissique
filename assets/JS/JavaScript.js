40c38352810b7ef6528012271cc395265cd3bce6
Elves Chissique <pedrochissique@hotmail.com>
2023-11-27 20:08:50 +0200

    updating

--- assets/JS/JavaScript.js
+++ assets/JS/JavaScript.js
@@ -165,7 +165,7 @@ function mudarParaFoto3(){
 
 /*Adicionando o Eventos e funções para a primeira div de serviços de delivery*/
 delivery1.addEventListener('mouseover',mudarParaTextoDelivery)
-delivery2.addEventListener('mouseout',mudarParaFotoDelivery)
+delivery1.addEventListener('mouseout',mudarParaFotoDelivery)
 
 function mudarParaTextoDelivery(){
     deliveryFoto1.style.width = '0px'
@@ -202,4 +202,90 @@ function mudarParaFotoDelivery(){
 
     deliveryH1.style.color = "#000"
     deliveryH1.style.transition = '0.5s'
+}
+
+
+
+/*Adicionando o Eventos e funções para a segunda div de serviços de delivery*/
+delivery2.addEventListener('mouseover',mudarParaTextoDelivery2)
+delivery2.addEventListener('mouseout',mudarParaFotoDelivery2)
+
+function mudarParaTextoDelivery2(){
+    deliveryFoto2.style.width = '0px'
+    deliveryFoto2.style.hight = '0px'
+    deliveryFoto2.style.opacity = '0'
+    deliveryFoto2.style.border = 'none'
+    deliveryFoto2.style.transition = '0.5s'
+
+
+    deliveryText2.style.fontSize = '13.1pt'
+    deliveryText2.style.backgroundColor = 'rgba(0,0,0,0.7)'
+    deliveryText2.style.transition = '0.3s'
+    deliveryText2.style.width = '290px'
+    deliveryText2.style.hight = '300px'
+    deliveryText2.style.opacity = '1'
+    deliveryText2.style.border = '2px solid #088080'
+
+    deliveryH2.style.color = "#fff"
+    deliveryH2.style.transition = '0.5s'
+
+}
+
+function mudarParaFotoDelivery2(){
+    deliveryFoto2.style.width = '290px'
+    deliveryFoto2.style.opacity = '1'
+    deliveryFoto2.style.transition = '0.7s'
+    deliveryFoto2.style.hight = '200px'
+
+    deliveryText2.style.border = 'none'
+    deliveryText2.style.transition = '.3s'
+    deliveryText2.style.width = '290'
+    deliveryText2.style.hight = '0'
+    deliveryText2.style.fontSize = '0px'
+
+    deliveryH2.style.color = "#000"
+    deliveryH2.style.transition = '0.5s'
+}
+
+
+
+/*Adicionando o Eventos e funções para a terceira div de serviços de delivery*/
+delivery3.addEventListener('mouseover',mudarParaTextoDelivery3)
+delivery3.addEventListener('mouseout',mudarParaFotoDelivery3)
+
+function mudarParaTextoDelivery3(){
+    deliveryFoto3.style.width = '0px'
+    deliveryFoto3.style.hight = '0px'
+    deliveryFoto3.style.opacity = '0'
+    deliveryFoto3.style.border = 'none'
+    deliveryFoto3.style.transition = '0.5s'
+
+
+    deliveryText3.style.fontSize = '13.1pt'
+    deliveryText3.style.backgroundColor = 'rgba(0,0,0,0.7)'
+    deliveryText3.style.transition = '0.3s'
+    deliveryText3.style.width = '290px'
+    deliveryText3.style.hight = '300px'
+    deliveryText3.style.opacity = '1'
+    deliveryText3.style.border = '2px solid #088080'
+
+    deliveryH3.style.color = "#fff"
+    deliveryH3.style.transition = '0.5s'
+
+}
+
+function mudarParaFotoDelivery3(){
+    deliveryFoto3.style.width = '290px'
+    deliveryFoto3.style.opacity = '1'
+    deliveryFoto3.style.transition = '0.7s'
+    deliveryFoto3.style.hight = '200px'
+
+    deliveryText3.style.border = 'none'
+    deliveryText3.style.transition = '.3s'
+    deliveryText3.style.width = '290'
+    deliveryText3.style.hight = '0'
+    deliveryText3.style.fontSize = '0px'
+
+    deliveryH3.style.color = "#000"
+    deliveryH3.style.transition = '0.5s'
 }
